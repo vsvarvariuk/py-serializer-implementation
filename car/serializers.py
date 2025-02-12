c755740cc047e4cecb40af205299291e51c414d4
vavarvariuk <vasya.varvariuk.27@gmail.com>
2025-02-12 14:28:02 +0200

    solution

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,14 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from rest_framework import serializers
 
 
 class CarSerializer(serializers.Serializer):
-    pass
+    id = serializers.IntegerField(read_only=True)
+    manufacturer = serializers.CharField(max_length=64)
+    model = serializers.CharField(max_length=64)
+    horse_power = serializers.IntegerField(
+        validators=[MaxValueValidator(1914), MinValueValidator(1)]
+    )
+    is_broken = serializers.BooleanField()
+    problem_description = serializers.CharField(required=False)
+
--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from car.models import Car
+from car.serializers import CarSerializer
+from rest_framework.renderers import JSONRenderer
+import io
+from rest_framework.parsers import JSONParser
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(car)
+    json = JSONRenderer().render(serializer.data)
+    return json
+
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    stream = io.BytesIO(json)
+    data = JSONParser().parse(stream)
+    serializer = CarSerializer(data=data)
+    if serializer.is_valid():
+        car_instance = serializer.save()
+        return car_instance
+    else:
+        raise ValueError("Invalid data")
+
